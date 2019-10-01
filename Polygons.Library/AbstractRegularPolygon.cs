356b92d2c0a74aebc5f251cf46eb16ed1466494e
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-01 09:22:36 +0200

    Add project files.

---
+++ Polygons.Library/AbstractRegularPolygon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Polygons.Library
+{
+    public abstract class AbstractRegularPolygon
+    {
+        public int NumberOfSides { get; set; }
+        public int SideLength { get; set; }
+
+        public AbstractRegularPolygon(int numberOfSides, int sideLength)
+        {
+            NumberOfSides = numberOfSides;
+            SideLength = sideLength;
+        }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public abstract double GetArea();
+    }
+}
---
+++ Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Library
+{
+    public class ConcreteRegularPolygon
+    {
+
+        public int NumberOfSides { get; set; } 
+        public int SideLength { get; set; }
+
+
+        public ConcreteRegularPolygon (int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
